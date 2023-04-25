aed96ecbe4524947856b648c3d749e69d16bc83e
chamalaa <laachaimae95@gmail.com>
2023-04-25 09:57:52 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - a function that prints every minut
+ * , starting from 00:00 to 23:59
+ * Return: no returns
+ */
+void jack_bauer(void)
+{
+	int min, hr;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,4 @@
+#include "main.h"
+/**
+ * times_table - a function that prints the 9 times table, starting with 0
+ *
