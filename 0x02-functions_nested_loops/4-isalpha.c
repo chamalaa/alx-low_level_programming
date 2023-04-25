4f45132e418be1f864f06e8b049909c94cd1a01f
chamalaa <laachaimae95@gmail.com>
2023-04-25 04:56:08 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: is a letter, lowercase or uppercase
  * Return:1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
