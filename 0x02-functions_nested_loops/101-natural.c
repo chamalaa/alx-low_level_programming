e13ef7797faa61625a60efca468e8017f9cdd99a
chamalaa <laachaimae95@gmail.com>
2023-04-26 09:48:02 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - list all the natural numbers below 10
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || ((num % 5 == 0)
+
+				sum += num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
