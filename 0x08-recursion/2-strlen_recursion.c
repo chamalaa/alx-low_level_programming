64665e311ab35b286964f9ef9890008d677eab72
chamalaa <laachaimae95@gmail.com>
2023-05-10 10:12:04 -0700

    0x08-recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int length = 0;
-
-	while (*s != '\0')
+	if (*s == '\0')
 	{
-		length++;
-		s++;
+		return (0);
 	}
-	return (length);
+	else
+	{
+		return ( 1 + _strlen_recursion(s + 1);
+	}
 }
