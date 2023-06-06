a2defa52e6103135faba0e7565eac331080dd0e9
chamalaa <laachaimae95@gmail.com>
2023-06-06 03:38:29 -0700

    lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * get_nodeint_at_index - returns the node at a certain index
- * @head: first node 
+ * @head: first node
  * @index: index of the node to return
  * Return: pointer to the node or NULL
  */
@@ -11,6 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *node;
 	unsigned int x;
 
-	for (node = head, x = 0; node && x < index; node = node->next, x++);
-       return (node);
-}       
+	for (node = head, x = 0; node && x < index; node = node->next, x++)
+		;
+	return (node);
+}
