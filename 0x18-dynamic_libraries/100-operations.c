3bf41db85aa4c089b035be98e46d67d511d8a2fc
chamalaa <laachaimae95@gmail.com>
2023-08-21 15:36:21 -0700

    dynamic

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,27 +1,69 @@
 #include <stdio.h>
+/**
+* add - adds two integers
+* @a: first integer to add
+* @b: second integer to add
+* Return: the sum
+*/
 
-int add(int a, int b){
-	return a + b;
+int add(int a, int b)
+{
+	return (a + b);
 }
 
-int sub(int a, int b){
-	return a - b;
+/**
+* sub - subtracts two integers
+* @a: first integer to add
+* @b: second integer to add
+* Return: the subtract
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
 }
 
-int div(int a, int b){
-	if (b == 0){
+/**
+* div - divideds two integers
+* @a: first integer to add
+* @b: second integer to add
+* Return: the dividend integer
+*/
+
+int div(int a, int b)
+{
+	if (b == 0)
+	{
 		printf("error: division by zero\n");
-		return 0;}
-	return a / b;
+		return (0);
+	}
+	return (a / b);
 }
 
-int mult(int a, int b){
-	return a * b;
+/**
+* mul - multiplies two integers
+* @a: first integer to add
+* @b: second integer to add
+* Return: the multiple
+*/
+int mult(int a, int b)
+{
+	return (a * b);
 }
 
-int mod(int a, int b){
-	if (b == 0){
+/**
+ * mod - finds the modulus of two integers
+ * @a: first integer to add
+ * @b: second integer to add
+ * Return: the modulus
+*/
+
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
 		printf("error: division by zero\n");
-                return 0;}
-	return a % b;
+		return (0);
+	}
+	return (a % b);
 }
